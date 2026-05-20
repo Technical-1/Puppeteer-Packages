940a21aeda25f2703f48c7f15c67add9a9f20219
Jacob Kanfer <kanfer@users.noreply.github.com>
2026-05-20 13:33:58 -0400

    feat(human): expose public surface; verify dual build

--- packages/human/src/index.ts
+++ packages/human/src/index.ts
@@ -1,1 +1,8 @@
-export {};
+export { humanDelay, humanType, humanMouseMove } from "./human.js";
+export type {
+  RandomFn,
+  DelayOptions,
+  TypeOptions,
+  MousePoint,
+  MouseMoveOptions,
+} from "./human.js";
